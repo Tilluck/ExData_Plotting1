688c57c6d1981763a8d926a6baa42574fc0ffa40
Tilluck <91812301+Tilluck@users.noreply.github.com>
2021-12-19 15:44:47 -0400

    The Third Graph

---
+++ plot3.R
@@ -0,0 +1,18 @@
+library("data.table")
+
+power_1 <- data.table::fread(input = "exdata_data_household_power_consumption/household_power_consumption.txt"
+                             , na.strings="?"
+)
+power_1[, date_Time := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+
+power_1 <- power_1[(date_Time >= "2007-02-01") & (date_Time < "2007-02-03")]
+
+png("plot3.png", width=480, height=480)
+
+plot(power_1[, date_Time], power_1[, Sub_metering_1], type = "l", xlab = "", ylab = "Energy sub metering")
+lines(power_1[, date_Time], power_1[, Sub_metering_2], col = "red")
+lines(power_1[, date_Time], power_1[, Sub_metering_3], col = "blue")
+legend("topright", col = c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  ")
+       ,lty = 1, lwd = 1)
+
+dev.off()
